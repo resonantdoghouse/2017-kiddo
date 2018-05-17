1c5f8a36594359f65057d5f04450c6104e0fdbc0
Jim <jim@advancebass.com>
2018-05-17 08:47:39 -0700

    remove composer.json, update function prefixes

--- functions.php
+++ functions.php
@@ -3,16 +3,16 @@
 /**
  * Actions Hooks & Filters
  */
-add_action( 'wp_enqueue_scripts', 'redsprout_enqueue_scripts' );
+add_action( 'wp_enqueue_scripts', 'kiddo_enqueue_scripts' );
 
 /**
  * Child Theme parent style & custom stylesheet
  */
-function redsprout_enqueue_scripts() {
+function kiddo_enqueue_scripts() {
 	// styles
 	wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
-	wp_enqueue_style( 'custom-style', get_stylesheet_directory_uri() . '/css/custom.css' );
+	wp_enqueue_style( 'kiddo-style', get_stylesheet_directory_uri() . '/css/custom.css' );
 	// scripts
 	wp_enqueue_script( 'jquery' );
-	wp_enqueue_script( 'redsprout', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ), false, true );
+	wp_enqueue_script( 'kiddo-script', get_stylesheet_directory_uri() . '/js/custom.js', array( 'jquery' ), false, true );
 }
